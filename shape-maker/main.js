34dacf371f8e753beacea26812256b0add5f81d8
martinDolan <martyopants@gmail.com>
2024-05-21 16:20:01 -0400

    Move files to vite

--- shape-maker/main.js
+++ shape-maker/main.js
@@ -1,24 +1,121 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// Parent class
+class Shape {
+  constructor(color) {
+    this.color = color;
+  }
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  createShape() {
+    const div = document.createElement("div");
+    div.className = "shape";
+    div.style.backgroundColor = this.color;
+    div.style.transition = "all 0.5s ease";
+    return div;
+  }
 
-setupCounter(document.querySelector('#counter'))
+  addEventListeners(div) {
+    div.addEventListener("mouseover", () => {
+      div.style.transform = "scale(1.1)";
+    });
+    div.addEventListener("mouseout", () => {
+      div.style.transform = "scale(1)";
+    });
+  }
+}
+
+// Child class for Rectangle
+class Rectangle extends Shape {
+  constructor(color, width, height) {
+    super(color);
+    this.width = width;
+    this.height = height;
+  }
+
+  createShape() {
+    const div = super.createShape();
+    div.style.width = this.width + "px";
+    div.style.height = this.height + "px";
+    super.addEventListeners(div);
+    return div;
+  }
+}
+
+// Child class for Circle
+class Circle extends Shape {
+  constructor(color, diameter) {
+    super(color);
+    this.diameter = diameter;
+  }
+
+  createShape() {
+    const div = super.createShape();
+    div.style.width = this.diameter + "px";
+    div.style.height = this.diameter + "px";
+    div.style.borderRadius = "50%";
+    super.addEventListeners(div);
+    return div;
+  }
+}
+
+// Child class for Triangle
+class Triangle extends Shape {
+  constructor(color, width, height) {
+    super(color);
+    this.width = width;
+    this.height = height;
+  }
+
+  createShape() {
+    const div = document.createElement("div");
+    div.className = "shape";
+    div.style.width = "0";
+    div.style.height = "0";
+    div.style.borderLeft = `${this.width / 2}px solid transparent`;
+    div.style.borderRight = `${this.width / 2}px solid transparent`;
+    div.style.borderBottom = `${this.height}px solid ${this.color}`;
+    div.style.transition = "all 0.5s ease";
+    super.addEventListeners(div);
+    return div;
+  }
+}
+
+// Generate random color
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+// Define shape configurations
+const shapeConfigurations = [
+  { type: "Rectangle", color: getRandomColor(), width: 150, height: 100 },
+  { type: "Rectangle", color: getRandomColor(), width: 200, height: 100 },
+  { type: "Rectangle", color: getRandomColor(), width: 150, height: 150 },
+  { type: "Circle", color: getRandomColor(), diameter: 100 },
+  { type: "Circle", color: getRandomColor(), diameter: 150 },
+  { type: "Triangle", color: getRandomColor(), width: 150, height: 150 },
+  { type: "Triangle", color: getRandomColor(), width: 100, height: 100 },
+];
+
+// Create instances and append them to the container
+const container = document.getElementById("container");
+
+shapeConfigurations.forEach((config) => {
+  let shape;
+  switch (config.type) {
+    case "Rectangle":
+      shape = new Rectangle(config.color, config.width, config.height);
+      break;
+    case "Circle":
+      shape = new Circle(config.color, config.diameter);
+      break;
+    case "Triangle":
+      shape = new Triangle(config.color, config.width, config.height);
+      break;
+  }
+  if (shape) {
+    container.appendChild(shape.createShape());
+  }
+});
