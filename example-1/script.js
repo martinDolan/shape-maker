a51b25b184157222e5e59853fa2052db877883ed
martinDolan <martyopants@gmail.com>
2024-05-21 15:02:22 -0400

    slightly simpler version

--- example-1/script.js
+++ example-1/script.js
@@ -1,44 +1,47 @@
-function BaseDiv(color, text) {
+// Base constructor function
+function Shape(color) {
   this.color = color;
-  this.text = text;
 }
 
-// Method to create a div
-BaseDiv.prototype.createDiv = function () {
+// Method to create a shape
+Shape.prototype.createShape = function () {
   const div = document.createElement("div");
-  div.className = "custom-div";
+  div.className = "shape";
   div.style.backgroundColor = this.color;
-  div.innerText = this.text;
+  div.style.width = "100px";
+  div.style.height = "50px";
   return div;
 };
 
 // Inherited constructor function
-function CustomDiv(color, text, borderRadius) {
-  BaseDiv.call(this, color, text);
-  this.borderRadius = borderRadius;
+function Rectangle(color, width, height) {
+  Shape.call(this, color); // Call the parent constructor
+  this.width = width;
+  this.height = height;
 }
 
-// Inherit the prototype of BaseDiv
-CustomDiv.prototype = Object.create(BaseDiv.prototype);
-CustomDiv.prototype.constructor = CustomDiv;
+// Inherit the prototype of Shape
+Rectangle.prototype = Object.create(Shape.prototype);
+Rectangle.prototype.constructor = Rectangle;
 
-// Override the createDiv method
-CustomDiv.prototype.createDiv = function () {
-  const div = BaseDiv.prototype.createDiv.call(this);
-  div.style.borderRadius = this.borderRadius;
+console.dir(Rectangle);
+
+// Override the createShape method
+Rectangle.prototype.createShape = function () {
+  const div = Shape.prototype.createShape.call(this);
+  div.style.width = this.width + "px";
+  div.style.height = this.height + "px";
   return div;
 };
 
 // Create instances and append them to the container
 const container = document.getElementById("container");
-
-const divs = [
-  new CustomDiv("red", "Div 1", "0px"),
-  new CustomDiv("blue", "Div 2", "10px"),
-  new CustomDiv("green", "Div 3", "20px"),
-  new CustomDiv("purple", "Div 4", "30px"),
+const shapes = [
+  new Rectangle("red", 150, 100),
+  new Rectangle("blue", 200, 100),
+  new Rectangle("green", 150, 150),
 ];
 
-divs.forEach((div) => {
-  container.appendChild(div.createDiv());
+shapes.forEach((shape) => {
+  container.appendChild(shape.createShape());
 });
